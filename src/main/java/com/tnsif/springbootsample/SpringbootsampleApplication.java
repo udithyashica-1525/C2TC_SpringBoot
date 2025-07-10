baec57730b8287088c2cb41da156382acd897953
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-07-10 09:27:36 +0530

    This is my springboot sample program

---
+++ src/main/java/com/tnsif/springbootsample/SpringbootsampleApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsampleApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
