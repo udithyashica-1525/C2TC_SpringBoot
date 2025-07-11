ce8cc8809be867ec0395722993ecd768ba7b81e3
udithyashica-1525 <udithyashi20@gmail.com>
2025-07-11 12:19:55 +0530

    This is my spring boot web app project

---
+++ WebApp/src/main/java/com/tnsif/WebApp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
