ffe6dc417309c344ac2e52a74cbae10c78e680b5
udithyashica-1525 <udithyashi20@gmail.com>
2025-07-11 12:19:13 +0530

    This is my spring boot sample project

---
+++ springbootsample/src/test/java/com/tnsif/springbootsample/SpringbootsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
